72db9d58df28bc67b49ef34cc7a21054cfeb9511
chrisaguero <102135611+chrisaguero@users.noreply.github.com>
2022-03-29 12:08:46 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+	<title>TurísTicos</title>
+	<link rel="shortcut icon" href="images/TurisTicos.png" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="assets/css/noscript.css" />
+	</noscript>
+</head>
+
+<body class="is-preload landing">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<header id="header">
+			<h1 id="logo"><a href="index.php">TurísTicos</a></h1>
+			<nav id="nav">
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li>
+						<a href="#">Destinos</a>
+						<ul>
+							<li><a href="left-sidebar.php">Inicio</a></li>
+							<li><a href="right-sidebar.php">Destinos</a></li>
+							<li><a href="no-sidebar.php">Blog</a></li>
+							<li>
+								<a href="#">Submenu</a>
+								<ul>
+									<li><a href="#">Option 1</a></li>
+									<li><a href="#">Option 2</a></li>
+									<li><a href="#">Option 3</a></li>
+									<li><a href="#">Option 4</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="elements.php">Contacto</a></li>
+					<li><a href="inicioSesion.php">Inicio Sesión</a></li>
+					<li><a href="registro.php" class="button primary">Registrarme</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<!-- Banner -->
+		<section id="banner">
+			<div class="content">
+				<header>
+					<h2>TurísTicos S.A.</h2>
+					<p>Tu próxima experiencia a un clik de distancia.</p>
+				</header>
+				<span class="image"><img src="images/pic01.jpg" alt="" /></span>
+
+
+				<div class="container">
+					<div class="lbl-menu">
+						<label for="radio1">Vuelo y Hotel</label>
+						<label for="radio2">Hospedaje</label>
+					</div>
+
+					<div class="content-01">
+						<input type="radio" name="radio" id="radio1">
+						<div class="tab1">
+							<table id="Tabla" class="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>
+											<span class="input-group-text" id="basic-addon1">Origen</span>
+											<input type="password" id="inputPassword" class="form-control" placeholder="Origen" aria-describedby="passwordHelpInline">
+										</th>
+										<th>
+											<span class="input-group-text" id="basic-addon1">Destino</span>
+											<input type="password" id="inputPassword" class="form-control" placeholder="¿Dónde deseas ir?" aria-describedby="passwordHelpInline">
+										</th>
+									</tr>
+								</thead>
+
+								<tbody>
+
+									<td>
+										<span class="input-group-text" id="basic-addon1">Fecha de regreso</span>
+										<input type="password" id="inputPassword" class="form-control" placeholder="Fecha de regreso" aria-describedby="passwordHelpInline">
+									</td>
+
+
+									<td>
+										<span class="input-group-text" id="basic-addon1">Fecha de salida</span>
+										<input type="password" id="inputPassword" class="form-control" placeholder="Fecha de salida" aria-describedby="passwordHelpInline">
+									</td>
+
+
+								</tbody>
+							</table>
+
+							<span class="input-group-text" id="basic-addon1">Habitaciones y pasajeros</span>
+							<input type="password" id="inputPassword" class="form-control" placeholder="Habitaciones y pasajeros" aria-describedby="passwordHelpInline">
+							<span class="input-group-text" id="basic-addon1">Clase</span>
+							<input type="password" id="inputPassword" class="form-control" placeholder="Clase" aria-describedby="passwordHelpInline">
+						</div>
+
+			<input type="radio" name="radio" id="radio2">
+			<div class="tab2">
+			<table id="Tabla" class="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>
+											<span class="input-group-text" id="basic-addon1">Origen</span>
+											<input type="password" id="inputPassword" class="form-control" placeholder="Origen" aria-describedby="passwordHelpInline">
+
+										</th>
+										<th>
+											<span class="input-group-text" id="basic-addon1">Destino</span>
+											<input type="password" id="inputPassword" class="form-control" placeholder="¿Dónde deseas ir?" aria-describedby="passwordHelpInline">
+
+										</th>
+
+									</tr>
+								</thead>
+
+								<tbody>
+
+									<td>
+										<span class="input-group-text" id="basic-addon1">Fecha de regreso</span>
+										<input type="password" id="inputPassword" class="form-control" placeholder="Fecha de regreso" aria-describedby="passwordHelpInline">
+									</td>
+
+
+									<td>
+										<span class="input-group-text" id="basic-addon1">Fecha de salida</span>
+										<input type="password" id="inputPassword" class="form-control" placeholder="Fecha de salida" aria-describedby="passwordHelpInline">
+									</td>
+
+
+								</tbody>
+							</table>
+
+							<span class="input-group-text" id="basic-addon1">Habitaciones y pasajeros</span>
+							<input type="password" id="inputPassword" class="form-control" placeholder="Habitaciones y pasajeros" aria-describedby="passwordHelpInline">
+							<span class="input-group-text" id="basic-addon1">Clase</span>
+							<input type="password" id="inputPassword" class="form-control" placeholder="Clase" aria-describedby="passwordHelpInline">
+		
+		</div>
+	</div>
+
+	<a href="#one" class="goto-next scrolly">Next</a>
+
+	</section>
+
+
+	<!-- One -->
+	<section id="one" class="spotlight style1 bottom">
+		<span class="image fit main"><img src="images/pic02.jpg" alt="" /></span>
+		<div class="content">
+			<div class="container">
+				<div class="row">
+					<div class="col-4 col-12-medium">
+						<header>
+							<h2>Odio faucibus ipsum integer consequat</h2>
+							<p>Nascetur eu nibh vestibulum amet gravida nascetur praesent</p>
+						</header>
+					</div>
+					<div class="col-4 col-12-medium">
+						<p>Feugiat accumsan lorem eu ac lorem amet sed accumsan donec.
+							Blandit orci porttitor semper. Arcu phasellus tortor enim mi
+							nisi praesent dolor adipiscing. Integer mi sed nascetur cep aliquet
+							augue varius tempus lobortis porttitor accumsan consequat
+							adipiscing lorem dolor.</p>
+					</div>
+					<div class="col-4 col-12-medium">
+						<p>Morbi enim nascetur et placerat lorem sed iaculis neque ante
+							adipiscing adipiscing metus massa. Blandit orci porttitor semper.
+							Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
+							mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
+							ipsum dolor nullam.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a href="#two" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- Two -->
+	<section id="two" class="spotlight style2 right">
+		<span class="image fit main"><img src="images/pic03.jpg" alt="" /></span>
+		<div class="content">
+			<header>
+				<h2>Interdum amet non magna accumsan</h2>
+				<p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
+			</header>
+			<p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+			<ul class="actions">
+				<li><a href="#" class="button">Learn More</a></li>
+			</ul>
+		</div>
+		<a href="#three" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- Three -->
+	<section id="three" class="spotlight style3 left">
+		<span class="image fit main bottom"><img src="images/pic04.jpg" alt="" /></span>
+		<div class="content">
+			<header>
+				<h2>Interdum felis blandit praesent sed augue</h2>
+				<p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
+			</header>
+			<p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+			<ul class="actions">
+				<li><a href="#" class="button">Learn More</a></li>
+			</ul>
+		</div>
+		<a href="#four" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- Four -->
+	<section id="four" class="wrapper style1 special fade-up">
+		<div class="container">
+			<header class="major">
+				<h2>Te explicamos porque somos tu mejor opción</h2>
+				<p></p>
+			</header>
+			<div class="box alt">
+				<div class="row gtr-uniform">
+
+					<section class="col-3 col-6-medium col-12-xsmall">
+						<span class="icon solid alt major fa-comment"></span>
+						<h3>Accesible</h3>
+						<p>Promociones y ofertas seleccionadas para ti.</p>
+					</section>
+					<section class="col-3 col-6-medium col-12-xsmall">
+						<span class="icon solid alt major fa-flask"></span>
+						<h3>Rápido y sencillo</h3>
+						<p>Reserva tu viaje de forma rápida y sencilla desde cualquier dispositivo</p>
+					</section>
+					<section class="col-3 col-6-medium col-12-xsmall">
+						<span class="icon solid alt major fa-paper-plane"></span>
+						<h3>Cuenta con nosotros</h3>
+						<p>Agentes especializados te ayudarán a elegir y planificar tus vacaciones para tu disfrute</p>
+					</section>
+					<section class="col-3 col-6-medium col-12-xsmall">
+						<span class="icon solid alt major fa-file"></span>
+						<h3>Soporte</h3>
+						<p>Viaja tranquilo: te ofrecemos gratis asistencia 24 horas en destino los 365 días del año</p>
+					</section>
+				</div>
+			</div>
+			<footer class="major">
+				<ul class="actions special">
+					<li><a href="#" class="button">Reservar</a></li>
+				</ul>
+			</footer>
+		</div>
+	</section>
+
+	<!-- Five -->
+	<section id="five" class="wrapper style2 special fade">
+		<div class="container">
+			<header>
+				<h2>Quiero recibir ofertas</h2>
+				<p>Recibiré semanalmente ofertas y promociones pensadas exclusivamente para mi</p>
+			</header>
+			<form method="post" action="#" class="cta">
+				<div class="row gtr-uniform gtr-50">
+					<div class="col-8 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Introduce tu e-mail" /></div>
+					<div class="col-4 col-12-xsmall"><input type="submit" value="Suscribirme" class="fit primary" /></div>
+				</div>
+			</form>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<footer id="footer">
+		<ul class="icons">
+			<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+			<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+			<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+			<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+			<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+		</ul>
+		<ul class="copyright">
+			<li>&copy; TurísTicos S.A. Todos los derechos reservados.</li>
+		</ul>
+	</footer>
+
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/jquery.dropotron.min.js"></script>
+	<script src="assets/js/jquery.scrollex.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
